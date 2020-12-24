d5550d2f047fd5eb6b5ee9fd72f7654ebb74ddf0
sunhyisu3474 <sunhyisu3474@gmail.com>
2020-12-24 19:42:49 +0900

    dqwf
    Signed-off-by: sunhyisu3474 <sunhyisu3474@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function modeChange() {
-  if (document.querySelector('#modeChange').value === 'night') {
-    document.querySelector('body').style.backgroundColor = 'black'; document.querySelector('body').style.color = 'white'; value = 'day';
-  } else {
-    document.querySelector('body').style.backgroundColor = 'white'; document.querySelector('body').style.color = 'black'; value = 'night'
+var setList = {
+  setColor: function (color) {
+    var alist = document.querySelectorAll('a');
+    for (var i = 0; i < alist.length; i++) {
+      alist[i].style.color = color;
+    }
+  }
+}
+
+var setBody = {
+  modeChange: function (self) {
+    var target = document.querySelector('body');
+    if (self.value === 'night') {  /* 야간모드로 변경 */
+      target.style.backgroundColor = 'black';
+      target.style.color = 'white';
+      setList.setColor('white');
+      self.value = 'day';
+    } else {  /* 주간모드로 변경 */
+      target.style.backgroundColor = 'white';
+      target.style.color = 'black';
+      setList.setColor('black');
+      self.value = 'night';
+    }
   }
 }
