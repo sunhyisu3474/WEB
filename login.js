d1f9fe901d1e0bca10bd5f9264cc29ee17603462
sunhyisu3474 <sunhyisu3474@gmail.com>
2020-12-26 21:53:55 +0900

    fbLogin
    Signed-off-by: sunhyisu3474 <sunhyisu3474@gmail.com>

--- login.js
+++
@@ -1,7 +0,0 @@
-var body = {
-  getLogin: function (self) {
-    if (self.id === '15' && self.pw === '15') {
-      document.write("LOGIN SUCCESS!");
-    }
-  }
-}
---
+++ login/login.js
@@ -0,0 +1,7 @@
+var body = {
+  getLogin: function () {
+    if (id.value === '15' && pw.value === '15') {
+      alert('LOGIN SUCCESS!');
+    }
+  }
+}
