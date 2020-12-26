62d021fe468149cef797943923b15eb79d5cb0a0
sunhyisu3474 <sunhyisu3474@gmail.com>
2020-12-27 00:57:45 +0900

    dqwdq
    Signed-off-by: sunhyisu3474 <sunhyisu3474@gmail.com>

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ window.fbAsyncInit = function () {
     console.log(response);
     // statusChagneCallback(response);
     if (response.status === 'connected') {
-      document.querySelector('#authBtn').value = LOGOUT;
+      document.querySelector('#authBtn').value = 'LOGOUT';
     } else {
-      document.querySelector('#authBtn').value = LOGIN;
+      document.querySelector('#authBtn').value = 'LOGIN';
     }
   }
   FB.getLoginStatus(callback);
